08547c55437e29a9d1e0bafbe8819a7ed9dba982
Rachel Hoffman <tailsnick5@gmail.com>
2022-05-24 16:29:08 -0600

    fixed and added Players to web page tab. Also moved HomeController.

--- Quintrix Web App Core/Controllers/HomeController.cs
+++ Quintrix Web App Core/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Quintrix_Web_App_Core.Models
+namespace Quintrix_Web_App_Core.Controllers
 {
     public class HomeController : Controller
     {
